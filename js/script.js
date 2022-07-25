9f36beb346f04937460204049a129b00efc8d34b
Rickson Sousa <rickson.sousa@cdd.corp>
2022-07-25 00:13:35 -0300

    final adjusts

--- js/script.js
+++ js/script.js
@@ -1,13 +1,17 @@
+//Intial instructions
 const pokemonName = document.querySelector('.pokemon_name');
 const pokemonNumber = document.querySelector('.pokemon_number');
 const pokemonImage = document.querySelector('.pokemon_image');
 
+//Forms
 const formResult = document.querySelector('.form');
 const inputResult = document.querySelector('.input_search');
 
+//Buttons
 const buttonPrev = document.querySelector('.btn-prev');
 const buttonNext = document.querySelector('.btn-next');
 
+//Initial pokemon
 let searchPokemon = 1;
 
 //get ID of the pokemon and return this
@@ -16,9 +20,9 @@ const fetchPokemon = async (pokemon) => {
     
     if(APIResponse.status === 200){
         const data = await APIResponse.json();
-        return data
-    }
-}
+        return data;
+    };
+};
 //result of fetchPokemon and return pokemon into API
 const renderPokemon = async (pokemon) => {
     pokemonName.innerHTML = 'Loading...';
@@ -34,9 +38,9 @@ const renderPokemon = async (pokemon) => {
     else{
         pokemonImage.style.display = 'none';
         pokemonName.innerHTML = 'Not Found';
-        pokemonNumber.innerHTML = ''
-    }
-}
+        pokemonNumber.innerHTML = '';
+    };
+};
 //Inicialization of site
 renderPokemon(searchPokemon);
 
